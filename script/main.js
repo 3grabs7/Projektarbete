8cc5582a1e86e995f025d0a4e34af438bfde3c09
Robin Grabs <robinmgrabs@gmail.com>
2021-02-16 19:01:21 +0100

    fuck git

--- script/main.js
+++ script/main.js
@@ -9,7 +9,6 @@ async function gifFetch() {
   let memes = [];
   let response = await fetch(`https://api.imgflip.com/get_memes`);
   let json = await response.json();
-<<<<<<< HEAD
 
   Array.from(json.data.memes).forEach((e, i) => {
     let cmd = e.name.replace(" ", "").slice(0, 3);
@@ -30,7 +29,6 @@ async function gifFetch() {
 }
 
 function loadPage(memes) {
-<<<<<<< HEAD
   memes.forEach(m=> {
     let item = document.createElement('div');
 
@@ -48,43 +46,3 @@ function loadPage(memes) {
     document.body.append(item);
   })
 }
-=======
-  memes.forEach((m) => {
-    let item = document.createElement("div");
-
-    let img = document.createElement("img");
-    img.src = m.url;
-    item.append(img);
-
-    let name = document.createElement("h1");
-    name.innerHTML = memes.name;
-    item.append(name);
-
-    let cmd = document.createElement("p");
-    cmd.innerHTML = m.cmd;
-    item.append(cmd);
-  });
-}
-
-// /
-// function gifFetch() {
-//   // http://alpha-meme-maker.herokuapp.com/:page
-
-//   fetch(`https://api.imgflip.com/get_memes`) //Fetchar URL för nå API:n
-//     .then((r) => r.json()) //slänger in det i JSON format + inväntar så det laddas klart.
-//     .then((data) => {
-//       console.log(data);
-//       let title = document.querySelector(".title");
-
-//       i = 0;
-//       for (i == 0; i < 20; i++) {
-//         let name = data.data.memes[i].name;
-//         let urlMeme = data.data.memes[i].url;
-//         title.innerHTML = name;
-//         cr
-//         document.querySelector("img").src = urlMeme;
-//         console.log(name + urlMeme);
-//       }
-//     });
-// }
->>>>>>> f20e2118554ef89822366f96b3890bd42f1e26d5
