d0c2c3f96ff6057325e644875e30433cd5b9ae35
Christopher Bergman <cb.christopherbergman@gmail.com>
2021-02-16 17:55:40 +0100

    Api fetch for memes through imgflip - >array of 30 memes

--- script/main.js
+++ script/main.js
@@ -1,2 +1,15 @@
-const submitBtn = document.querySelector(".submit");
-const inputFromUser = document.querySelector(".search");
+const lookMemes = document.querySelector(".buttonOne");
+
+lookMemes.addEventListener("click", () => {
+  return gifFetch();
+});
+
+function gifFetch() {
+  // http://alpha-meme-maker.herokuapp.com/:page
+
+  fetch(`https://api.imgflip.com/get_memes`) //Fetchar URL för nå API:n
+    .then((r) => r.json()) //slänger in det i JSON format + inväntar så det laddas klart.
+    .then((data) => {
+      console.log(data);
+    });
+}
