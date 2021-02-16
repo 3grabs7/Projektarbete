ceab280c2a4083dfb03a7d03f811f9559b7d425f
Robin Grabs <robinmgrabs@gmail.com>
2021-02-16 18:55:29 +0100

    async method for api calls

--- script/main.js
+++ script/main.js
@@ -4,12 +4,39 @@ lookMemes.addEventListener("click", () => {
   return gifFetch();
 });
 
-function gifFetch() {
+async function gifFetch() {
   // http://alpha-meme-maker.herokuapp.com/:page
+  let memes = [];
+  let response = await fetch(`https://api.imgflip.com/get_memes`);
+  let json = await response.json();
 
-  fetch(`https://api.imgflip.com/get_memes`) //Fetchar URL för nå API:n
-    .then((r) => r.json()) //slänger in det i JSON format + inväntar så det laddas klart.
-    .then((data) => {
-      console.log(data);
+  Array.from(json.data.memes).forEach((e, i) => {
+    let cmd = e.name.replace(" ", "").slice(0, 3);
+    memes.push({
+      name: e.name ?? 'unknown',
+      cmd: memes.includes({cmd:cmd}) ? `${cmd}${i}` : cmd,
+      url: e.url,
     });
+  });
+  console.log(memes);
+  loadPage(memes);
+}
+
+function loadPage(memes) {
+  memes.forEach(m=> {
+    let item = document.createElement('div');
+
+    let img = document.createElement('img');
+    img.src = m.url;
+    item.append(img);
+
+    let name = document.createElement('h1');
+    name.innerHTML = m.name;
+    item.append(name);
+
+    let cmd = document.createElement('p');
+    cmd.innerHTML = m.cmd;
+    item.append(cmd);
+    document.body.append(item);
+  })
 }
