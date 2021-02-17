01cf827946931ae4e21bfd2cd1b3e679d1b2844f
Christopher Bergman <cb.christopherbergman@gmail.com>
2021-02-17 10:30:37 +0100

    trying to add stuff to msg bot

--- discord_bot/index.js
+++ discord_bot/index.js
@@ -1,9 +1,7 @@
-require('dotenv').config();
+require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.login(process.env.BOTTOKEN);
-
-
 
 client.on("ready", () => {
   console.log("Connected as " + client.user.tag);
@@ -16,23 +14,29 @@ client.on("message", (msg) => {
 });
 
 client.on("message", (msg) => {
-    if (msg.content === "björn") {
-      msg.channel.send("<3 <3 <3 <3 <3 <3 <3");
-    }
-  });
-
-client.on("message", (message) => {
-    const prefix = 'gif';
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
-    
-    if (command === prefix) {
-        if (!args.length) {
-            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-        }
-        message.channel.send(`Command name: ${command}\nArguments: ${args}`);
-    }
+  if (msg.content === "björn") {
+    msg.channel.send("<3 <3 <3 <3 <3 <3 <3");
+  }
 });
 
+client.on("message", (msg) => {
+  if (msg.content === "!lektion!") {
+    msg.channel.send("Tisdag 8 till 5, baaby!");
+  }
+});
 
+client.on("message", (message) => {
+  const prefix = "gif";
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === prefix) {
+    if (!args.length) {
+      return message.channel.send(
+        `You didn't provide any arguments, ${message.author}!`
+      );
+    }
+    message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+  }
+});
