5544ffbf38bd605cd14a4d58c16c3c98f2e24d47
Robin Grabs <robinmgrabs@gmail.com>
2021-02-18 17:05:14 +0100

    send json from browser to server at node.

--- script/main.js
+++ script/main.js
@@ -41,3 +41,15 @@ function clearContainer() {
 		e.remove()
 	);
 }
+
+document.querySelector('#server').addEventListener('click', async () => {
+	let post = await fetch('http://localhost:8124/', {
+		method: 'POST',
+		mode: 'no-cors',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ test: 'några grejer', test2: 'flera grejer' }),
+	});
+	console.log(post);
+});
