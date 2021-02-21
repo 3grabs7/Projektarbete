6c0651fc20d30405d48f34e490bd24745332ed5f
Robin Grabs <robinmgrabs@gmail.com>
2021-02-21 10:18:41 +0100

    error handling for gif command.

--- discord_bot/commands/gif.js
+++ discord_bot/commands/gif.js
@@ -1,32 +1,28 @@
 const fetch = require('node-fetch');
 
-// GIF Command
 module.exports = async (msg, args) => {
-	let searchTerm = args[0];
-
-	let results = await getGif(searchTerm);
-	let index = 0;
-	if (args[1] != undefined) {
-		if (args[1] - 1 >= 0 && args[1] <= results.length) {
-			index = args[1] - 1;
-		} else {
-			index = 0;
-		}
-	} else {
-		index = Math.floor(Math.random() * results.length);
-	}
-	let randomGif = results[index];
-	msg.channel.send(randomGif);
-};
-
-async function getGif(search_term) {
 	let apikey = 'IUK0U580FPAU';
-	var search_url = `https://g.tenor.com/v1/search?q=${search_term}&key=${apikey}&limit=70`;
+	var search_url = `https://g.tenor.com/v1/search?q=${args[0]}&key=${apikey}&limit=70`;
+
 	let response = await fetch(search_url);
-	let json = await response.json();
-	let array = json.results
-		.map((r) => r.media)
-		.map((a) => a[0])
-		.map((g) => g.gif.url);
-	return array;
-}
+	if (response.status === 200) {
+		let json = await response.json();
+		let results = json.results
+			.map((r) => r.media)
+			.map((a) => a[0])
+			.map((g) => g.gif.url);
+
+		let index = 0;
+		if (args[1] != undefined) {
+			if (args[1] - 1 >= 0 && args[1] <= results.length) {
+				index = args[1] - 1;
+			} else {
+				index = 0;
+			}
+		} else {
+			index = Math.floor(Math.random() * results.length);
+		}
+		let randomGif = results[index];
+		msg.channel.send(randomGif);
+	}
+};
--- discord_bot/commands/meme.js
+++ discord_bot/commands/meme.js
@@ -3,6 +3,7 @@ const fetch = require('node-fetch');
 module.exports = async (msg, args) => {
 	let memes = [];
 	let response = await fetch(`https://api.imgflip.com/get_memes`);
+
 	if (response.status === 200) {
 		let json = await response.json();
 		Array.from(json.data.memes).forEach((e, i) => {
@@ -14,22 +15,27 @@ module.exports = async (msg, args) => {
 				id: e.id,
 			});
 		});
+
 		let meme = await addTextToMeme(memes, args);
 		if (meme.length === 0) {
 			console.log('Invalid meme code entered');
 			return;
 		}
 		msg.channel.send(meme);
+		return;
 	}
+	console.log(`Server responded with error : ${response.statusText}`);
 };
 
 async function addTextToMeme(memes, args) {
 	let urlRoot = 'https://api.imgflip.com/caption_image?';
 	let cred = 'username=chrisgrabs&password=bjorn1337';
 	let id = memes.filter((m) => m.cmd === args[0]).map((m) => m.id);
+
 	for (let i = 1; i < args.length; i++) {
 		args[i] = args[i].split('-').join(' ');
 	}
+
 	let query = `&template_id=${id}&text0=${args[1] ?? ''}&text1=${
 		args[2] ?? ''
 	}`;
@@ -42,5 +48,5 @@ async function addTextToMeme(memes, args) {
 		}
 		return memes.filter((m) => m.cmd === args[0]).map((m) => m.url);
 	}
-	console.log(`Server responded with error : ${resonpse.statusText}`);
+	console.log(`Server responded with error : ${response.statusText}`);
 }
