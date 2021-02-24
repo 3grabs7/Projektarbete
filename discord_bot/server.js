9425b63540658d6b175a91c482636c68f957cc6e
Robin Grabs <robinmgrabs@gmail.com>
2021-02-24 21:17:42 +0100

    Added comments to server.

--- discord_bot/server.js
+++ discord_bot/server.js
@@ -2,11 +2,16 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 
+// vår server
 const server = http.createServer((req, res) => {
+	// splittar den requestade url precis som med args i commands
 	let args = req.url.split('/').slice(1);
-	console.log(args);
+	// vi kollar med första args vad för grejer folk vill ha
+	// localhost:5000/discord%20data
 	if (args[0] === 'discord%20data') {
 		fs.readFile(
+			// vi letar i vår public folder efter args[1].json
+			// localhost:5000/discord%20data/test
 			path.join(__dirname, 'public', `${args[1]}.json`),
 			(err, content) => {
 				res.writeHead(200, { 'Content-Type': 'application/json' });
@@ -15,8 +20,11 @@ const server = http.createServer((req, res) => {
 		);
 		return;
 	}
+	// nästa för att hämta bilder
+	// localhost:5000/memebank
 	if (args[0] === 'memebank') {
 		fs.readFile(
+			// hämtar tex steffo med localhost:5000/memebank/steffo
 			path.join(__dirname, 'uploadedimgs', `${args[1]}.jpg`),
 			(err, content) => {
 				res.writeHead(200, { 'Content-Type': 'image/jpg' });
@@ -25,6 +33,7 @@ const server = http.createServer((req, res) => {
 		);
 		return;
 	}
+	// om filepath inte finns får vi nöjet att äntligen vara den som skickar iväg den här vidriga siffran
 	res.writeHead(404, 'Du vet vad det betyder');
 	res.end();
 });
