0655567fc463d01bcc41056721fd476dc68a92eb
Christopher Bergman <cb.christopherbergman@gmail.com>
2021-02-24 14:47:08 +0100

    some design to meme-text for new meme img

--- discord_bot/commands/ownmeme.js
+++ discord_bot/commands/ownmeme.js
@@ -1,3 +1,5 @@
 module.exports = function (msg, args) {
-	msg.reply(`https://abinkpoo.sirv.com/path/to/${args[0]}.jpg?text=${args[1]}`);
+	msg.reply(
+		`https://abinkpoo.sirv.com/path/to/${args[0]}.jpg?text=${args[1]}&text.color=FFFFFF&text.font.weight=extra-bold&text.font.size=28&text.position.gravity=center&text.position.y=50`
+	);
 };
