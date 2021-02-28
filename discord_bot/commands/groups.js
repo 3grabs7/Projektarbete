0c51bb30d297abc15ce411c45ad2fd7b6bdc5718
Robin Grabs <robinmgrabs@gmail.com>
2021-02-28 17:50:18 +0100

    bot command finds users group

--- discord_bot/commands/groups.js
+++ discord_bot/commands/groups.js
@@ -1,15 +1,11 @@
-const userIds = {
-	Robin: 123,
-	Nånannan: 321,
-};
-
 module.exports = function (msg, args) {
+	const userIds = JSON.parse(localStorage.getItem('userids.json')).users;
 	const json = JSON.parse(localStorage.getItem('groups.json'));
 
 	if (!args[0]) {
 		let response = '**Groups**\n';
 		json.groups.forEach((group) => {
-			response += `Grupp ${group.group} - `;
+			response += `Grupp ${group.groupId} - `;
 			group.members.forEach((member) => (response += `${member}, `));
 			response = response.slice(0, response.length - 2);
 			response += `\n`;
@@ -17,14 +13,22 @@ module.exports = function (msg, args) {
 		msg.channel.send(response);
 		return;
 	}
-	let user = ''; //* Find name from userid in object userIds
-	let usersGroup = 0;
+
+	let usersGroup = -1;
+	let authorId = msg.author.toString().replace(/[^0-9]/g, '');
+	let userName = userIds.filter((user) => user.id === authorId)[0].name;
+
 	json.groups.forEach((g) => {
 		g.members.forEach((m) => {
-			if (m === user) {
-				usersGroup = g;
+			if (m.toLowerCase() === userName.toLowerCase()) {
+				usersGroup = g.groupId;
 			}
 		});
 	});
+
+	if (usersGroup === -1) {
+		msg.reply('You got no group man, sorry');
+		return;
+	}
 	msg.reply(`Your in group ${usersGroup}, stop stalling`);
 };
